33d9ea2bc64104457df32977010f079ca328aecb
mykomiparanum <m.miparanum@gmail.com>
2024-01-27 16:02:13 +0100

    connect to db

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoUri: any = process.env.mongo_uri;
+mongoose
+  .connect(mongoUri)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err: any) => {
+    console.log(err);
+  });
 
 const app = express();
 const port = 3000;
