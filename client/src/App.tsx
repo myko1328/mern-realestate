7149a01f188533f144d3c1a01c71e2a04081e77f
mykomiparanum <m.miparanum@gmail.com>
2024-01-27 11:45:47 +0100

    test commit

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/pages/Signin.tsx
@@ -0,0 +1,5 @@
+const Signin = () => {
+  return <div>singin ni</div>;
+};
+
+export default Signin;
