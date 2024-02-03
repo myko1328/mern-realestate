b32d3581e3a6e3cf29baab82afcfb7a49777bcb8
mykomiparanum <m.miparanum@gmail.com>
2024-02-03 14:03:13 +0100

    sign in api route

--- api/controllers/auth.controller.ts
+++ api/controllers/auth.controller.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import bcrypts from "bcryptjs";
+import dotenv from "dotenv";
 import User from "../models/user.model";
 import { errorHandler } from "../utlis/error";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+const jwtToken: any = process.env.JWT_SECRET;
 
 export const signup = async (
   req: Request,
@@ -19,6 +24,43 @@ export const signup = async (
     res
       .status(201)
       .json({ success: true, message: "User created successfully!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email: email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "Wrong credentials!11"));
+    }
+
+    const validPassword = bcrypts.compareSync(password, validUser.password);
+
+    if (!validPassword) return next(errorHandler(404, "Wrong credentials!"));
+
+    const token = jwt.sign({ id: validUser._id }, jwtToken);
+
+    const user = {
+      _id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+      createdAt: validUser.createdAt,
+      updatedAt: validUser.updatedAt,
+    };
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(user);
   } catch (error) {
     next(error);
   }
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -37,7 +37,7 @@ const Signup = () => {
       setError(error.message);
     }
   };
-
+  console.log({ formData });
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
