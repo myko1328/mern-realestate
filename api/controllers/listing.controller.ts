09631cf3271d1952c5165791cacb52f0b40be484
mykomiparanum <m.miparanum@gmail.com>
2024-02-18 00:45:54 +0100

    feat: get listing api, update listing UI

--- api/controllers/listing.controller.ts
+++ api/controllers/listing.controller.ts
@@ -70,3 +70,19 @@ export const updateListing = async (
     next(error);
   }
 };
+
+export const getListing = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
