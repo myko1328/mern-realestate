95d4828f3bc376b80bcc340b0de31fec1a3a7c20
mykomiparanum <m.miparanum@gmail.com>
2024-02-22 23:16:44 +0100

    fix dir name

--- api/index.ts
+++ api/index.ts
@@ -33,10 +33,10 @@ app.use("/api/v1/user", userRouter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/listing", listingRouter);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(path.resolve(), "/client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(path.resolve(), "client", "dist", "index.html"));
 });
 
 app.use((err: any, req: any, res: any, next: any) => {
