c9d33526e5f07757c20923779c5dd8681d5d9ccb
mykomiparanum <m.miparanum@gmail.com>
2024-02-22 16:44:35 +0100

    feat: search listing api

--- api/controllers/listing.controller.ts
+++ api/controllers/listing.controller.ts
@@ -86,3 +86,89 @@ export const getListing = async (
     next(error);
   }
 };
+
+interface QueryParams {
+  limit?: string;
+  startIndex?: string;
+  offer?:
+    | {
+        $in: string | boolean[];
+      }
+    | string;
+  furnished?:
+    | {
+        $in: string | boolean[];
+      }
+    | string;
+  parking?:
+    | {
+        $in: string | boolean[];
+      }
+    | string;
+  type?:
+    | {
+        $in: string[];
+      }
+    | string;
+  searchTerm?: string;
+  sort?: string;
+  order?: string;
+}
+
+export const getListings = async (
+  req: Request<{}, {}, {}, QueryParams>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const limit: number = parseInt(req.query.limit ?? "0") || 9;
+    const startIndex: number = parseInt(req.query.startIndex ?? "0") || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const sortCriteria: { [key: string]: "asc" | "desc" } = {};
+    sortCriteria[sort] = order as "asc" | "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort(sortCriteria)
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
