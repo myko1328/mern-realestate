fe8db6805275746d33b2a95fe1f2dac7a854dcac
mykomiparanum <m.miparanum@gmail.com>
2024-04-15 18:29:33 +0200

    chore: remove localhost url

--- api/src/__test__/route/health.test.ts
+++ api/src/__test__/route/health.test.ts
@@ -5,14 +5,11 @@ describe("Health Route", () => {
   // Clean up after tests
   afterAll(async () => {
     // Clear all jest mocks
-    jest.clearAllMocks();
     test_server.close();
   });
 
   it("user should be able to get user details", async () => {
-    const response = await request(test_server).get(
-      `http://localhost:8080/health`
-    );
+    const response = await request(test_server).get(`/health`);
 
     expect(response.status).toBe(200);
   }, 50000);
